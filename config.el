593ce57a788bfb86511f3359b540817edcdd955b
Yecine Megdiche <megdiche@in.tum.de>
2019-12-27 00:22:32 +0100

    clean re-start

--- config.el
+++ config.el
@@ -1,57 +1,69 @@
-;;; .doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Yecine Megdiche"
+      user-mail-address "yecine.megdiche@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; test
+(setq doom-font (font-spec :family "Source Code Pro" :size 16)
+      doom-variable-pitch-font (font-spec :family "sans"))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-molokai)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type `absolute)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;; Company
 (setq company-idle-delay 0)
-(require 'doom-themes)
 
-(load-theme 'doom-molokai t)
-(setq doom-font "Source Code Pro:pixelsize=16")
-(setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-(doom-themes-treemacs-config)
-
-;; Corrects (and improves) org-mode's native fontification.
-(doom-themes-org-config)
-;; UI
-(setq-default truncate-lines nil)
-(setq doom-line-numbers-style 'relative)
-(global-whitespace-mode -1)
-;;MAGIT
-(add-hook 'after-save-hook 'magit-after-save-refresh-status t)
-
-;; Undo Granularity
-(advice-add 'undo-auto--last-boundary-amalgamating-number :override #'ignore)
-
-;;TRAMP SETTINGS
-
-
-;; Flycheck
-(after! flycheck (setq flycheck-check-syntax-automatically '(mode-enabled idle-change)))
-
-;; pdf-tools
+;; PDF-INFO
 (setq pdf-view-resize-factor 1.1)
+;; midnight-mode colors :dark grey, #012B36
+(after! pdf-view-mode (setq pdf-view-midnight-colors '("dark gray" . "#012B36")))
 
-;; EShell
-(setq-hook! 'eshell-mode-hook company-idle-delay nil)
+;;Flycheck
+(after! flycheck (setq flycheck-check-syntax-automatically '(mode-enabled idle-change)) (setq flycheck-idle-change-delay '1))
 
-;; Irony
-(setq irony-server-install-prefix '"/usr/")
- (add-hook 'c++-mode-hook 'irony-mode)
- (add-hook 'c-mode-hook 'irony-mode)
- (add-hook 'objc-mode-hook 'irony-mode)
+;; UI
+(setq truncate-lines nil)
 
- (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-;; Haskell
-
-;;(add-hook 'haskell-mode-hook 'dante-mode)
-
-;; (use-package dante
-;;   :ensure t
-;;   :after haskell-mode
-;;   :commands 'dante-mode
-;;   :init
-;;   (add-hook 'haskell-mode-hook 'flycheck-mode)
-;;   ;; OR:
-;;   ;; (add-hook 'haskell-mode-hook 'flymake-mode)
-;;   (add-hook 'haskell-mode-hook 'dante-mode)
-;;   )
+;; Python
+(add-to-list '+format-on-save-enabled-modes 'python-mode 't)
--- init.el
+++ init.el
@@ -36,12 +36,13 @@
         +defaults)       ; default popup rules
        pretty-code       ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
+       ;;zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -49,7 +50,7 @@
        fold              ; (nigh) universal code folding
        (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
-       ;;lispy             ; vim for lisp, for people who dont like vim
+       ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
@@ -64,7 +65,7 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell            ; a consistent, cross-platform shell (WIP)
+       ;;eshell            ; a consistent, cross-platform shell (WIP)
        ;;shell             ; a terminal REPL for Emacs
        ;;term              ; terminals in Emacs
        ;;vterm             ; another terminals in Emacs
@@ -93,12 +94,11 @@
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
-       ;;wakatime
 
        :lang
        ;;agda              ; types of types of types of types...
-       ;;assembly          ; assembly for fun or debugging
-       ;;cc                ; C/C++/Obj-C madness
+       assembly          ; assembly for fun or debugging
+       cc                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -113,15 +113,16 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
        ;;go                ; the hipster dialect
-       (haskell +lsp) ; a language that's lazier than I am
+       (haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
-       (java +meghanada) ; the poster child for carpal tunnel syndrome
+       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
        ;;lean
+       ;;factor
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
@@ -130,19 +131,20 @@
        ;;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +dragndrop       ; drag & drop files/images into org buffers
-        ;+hugo            ; use Emacs for hugo blogging
-        +ipython         ; ipython/jupyter support for babel
-        +pandoc          ; export-with-pandoc support
-        ;+pomodoro        ; be fruitful with the tomato technique
+        ;;+hugo            ; use Emacs for hugo blogging
+        +jupyter        ; ipython/jupyter support for babel
+        ;;+pandoc          ; export-with-pandoc support
+        ;;+pomodoro        ; be fruitful with the tomato technique
         +present)        ; using org-mode for presentations
        ;;perl              ; write code no one else can comprehend
-       ;;php               ; perl's insecure younger brother
+       php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +conda)           ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
+       ;;rst               ; ReST in peace
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
@@ -154,78 +156,27 @@
        ;;web               ; the tubes
 
        :email
-       ;;(mu4e +gmail)       ; WIP
-       ;;notmuch             ; WIP
-       ;;(wanderlust +gmail) ; WIP
+       ;;(mu4e +gmail)
+       ;;notmuch
+       ;;(wanderlust +gmail)
 
-       ;; Applications are complex and opinionated modules that transform Emacs
-       ;; toward a specific purpose. They may have additional dependencies and
-       ;; should be loaded late.
        :app
        ;;calendar
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
-       ;;(write            ; emacs for writers (fiction, notes, papers, etc.)
-       ;; +wordnut         ; wordnet (wn) search
-       ;; +langtool)       ; a proofreader (grammar/style check) for Emacs
+       ;;write             ; emacs for writers (fiction, notes, papers, etc.)
 
        :config
-       ;; For literate config users. This will tangle+compile a config.org
-       ;; literate config in your `doom-private-dir' whenever it changes.
        ;;literate
-
-       ;; The default module sets reasonable defaults for Emacs. It also
-       ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
-       ;; config. Use it as a reference for your own modules.
        (default +bindings +smartparens))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(+popup-mode t)
- '(company-backends (quote (company-eclim)))
- '(eclim-eclipse-dirs (quote ("/usr/lib/eclipse")))
- '(flycheck-popup-tip-error-prefix "✕ ")
- '(haskell-interactive-popup-errors nil)
- '(meghanada-class-completion-matcher "fuzzy")
- '(pdf-view-continuous t)
- '(pdf-view-midnight-colors (quote ("gainsboro" . "#002b36")))
- '(which-key-custom-hide-popup-function (quote which-key--hide-buffer-side-window))
- '(which-key-custom-popup-max-dimensions-function
-   (quote
-    (closure
-     (t)
-     (_)
-     (which-key--side-window-max-dimensions))))
- '(which-key-custom-show-popup-function
-   (quote
-    (closure
-     (t)
-     (act-popup-dim)
-     (let*
-         ((vnew
-           (function
-            (lambda
-              (buffer alist)
-              (+popup-display-buffer-stacked-side-window-fn buffer
-                                                            (append
-                                                             (quote
-                                                              ((vslot . -9999)))
-                                                             alist)))))
-          (old
-           (symbol-function
-            (quote display-buffer-in-side-window))))
-       (unwind-protect
-           (progn
-             (fset
-              (quote display-buffer-in-side-window)
-              vnew)
-             (which-key--show-buffer-side-window act-popup-dim))
-         (fset
-          (quote display-buffer-in-side-window)
-          old)))))))
+ '(midnight-mode t)
+ '(pdf-view-midnight-colors (quote ("dark gray" . "#012B36"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
